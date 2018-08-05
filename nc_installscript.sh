c71231c992c204b0e4b29c7e4dd2fe81a430ac59
s0544505 <41302961+s0544505@users.noreply.github.com>
2018-08-06 01:34:58 +0200

    Add files via upload

--- nc_installscript.sh
+++ nc_installscript.sh
@@ -14,20 +14,20 @@ read -p 'Please provide a password for the new User: ' newuserpasswordvar
 read -p 'Please provide a password for the PostreSQL-Database. Use a very strong one!: ' postgresqlpwvar
 read -p 'Please provide the Domain to your new Nextcloud (usually cloud.yourdomain.com): ' ncdomainvar
 read -p 'Please provide the Domain for your new Collabora-CODE (usually office.yourdomain.com): ' officedomainvar
-read -p 'Please provide a working mailaddress to receive securityconserned mails from Letsencrypt: ' lemailvar
+read -p 'Please provide a working mailaddress to receive security-related mails from Letsencrypt: ' lemailvar
 
 # remove the known host, ifn needed
 #sudo ssh-keygen -f "/home/$User/.ssh/known_hosts" -R "$serveraddress"
 
 echo "Please enter the root passwort for your server, then choose yes and hit [Return] when aksed to.."
 #ssh to remote, login as root, create new user and enable sudo
-ssh -tY $rootuservar@$serveraddressvar "echo 'Creating new User...'; sudo useradd $newuservar -d /home/$newuservar -m; sudo usermod -aG sudo $newuservar; usermod -s /bin/bash $newuservar; sudo echo $newuservar:$newuserpasswordvar | sudo chpasswd; echo 'preparing docker group...'; sudo groupadd docker ; sudo usermod -aG docker $newuservar;"
+ssh -t $rootuservar@$serveraddressvar "echo 'Creating new User...'; sudo useradd $newuservar -d /home/$newuservar -m; sudo usermod -aG sudo $newuservar; usermod -s /bin/bash $newuservar; sudo echo $newuservar:$newuserpasswordvar | sudo chpasswd; echo 'preparing docker group...'; sudo groupadd docker ; sudo usermod -aG docker $newuservar;"
 
 echo "Will try to login as $newuservar, please provide the password when promted and hit [Return]:"
 #ssh to remote with prviously created user  user, install git, docker, enable sudo for docker deamon, install docker-compose, clone git repository with env-files and docker-compose-file, replace environmet variables with userinputs via SED, create container with docker-compose
-ssh -tY $newuservar@$serveraddressvar "echo 'Successfully logged in as user $newuservar, now its required to enter $newuservar s password again. this is necessary to run in sudo:';
-sudo apt-get update -y --no-install-recommends; echo '..installing Git'; sudo apt-get install git -y --no-install-recommends;
-sudo apt-get update -y --no-install-recommends; echo '..installing docker'; sudo apt-get -y --no-install-recommends install curl apt-transport-https ca-certificates software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' && sudo apt-get -y --no-install-recommends install docker-ce;
+ssh -t $newuservar@$serveraddressvar "echo 'Successfully logged in as user $newuservar, now its required to enter $newuservar s password again. this is necessary to run in sudo:';
+sudo apt-get update -yq; echo '..installing Git'; sudo apt-get install git -yq --no-install-recommends;
+sudo apt-get update -yq; echo '..installing docker'; sudo apt-get -yq --no-install-recommends install curl apt-transport-https ca-certificates software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' && sudo apt-get -y --no-install-recommends install docker-ce;
 echo 'installing docker-compose'; sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose; echo 'Applying executable permissions to the docker-compose binary'; 
 sudo chmod +x /usr/local/bin/docker-compose;
 echo 'cloning the needed git repository'; git clone https://github.com/s0544505/nc-collabora-docker-compose.git; cd nc-collabora-docker-compose;
